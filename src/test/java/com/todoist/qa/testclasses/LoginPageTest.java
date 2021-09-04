46b2a245d6d01dcba6649cdb4d8cc436a864d9c2
Moinali5599 <59311134+Moinali5599@users.noreply.github.com>
2021-09-04 18:04:25 +0530

    Update LoginPageTest.java

--- src/test/java/com/todoist/qa/testclasses/LoginPageTest.java
+++ src/test/java/com/todoist/qa/testclasses/LoginPageTest.java
@@ -19,17 +19,17 @@ public class LoginPageTest extends PageBase{
 	
 	@Test(priority = 1)
 	public void loginWithEmailPassword() {
-		loginpage.doLogin("moinali5599@gmail.com", "MoinAli@5599");
+		loginpage.doLogin("email", "password");
 	}
 	
 	@Test(priority = 2)
 	public void loginWithIncorrectPassword() {
-		loginpage.doLogin("moinali5599@gmail.com", "Moin@1999");
+		loginpage.doLogin("mail", "password");
 	}
 	
 	@Test(priority = 3)
 	public void loginWithIncorrectEmail() {
-		loginpage.doLogin("incorrectmail", "MoinAli@5599");
+		loginpage.doLogin("incorrectmail", "password");
 	}
 	
 	@AfterMethod
